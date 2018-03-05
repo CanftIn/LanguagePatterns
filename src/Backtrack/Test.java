d03168d3a8390b646a0b0b3af2a86fa703b53dc0
CanftIn <wwc7033@gmail.com>
2018-03-05 15:38:17 +0800

    add Backtrack parser

--- src/Backtrack/Test.java
+++ src/Backtrack/Test.java
@@ -1,5 +1,10 @@
 package Backtrack;
 
 public class Test {
-
+    public static void main(String[] args) throws RecognitionException {
+        BacktrackLexer lexer = new BacktrackLexer("[test = t, a, b, [c, d, [e, f], g], hh = hhh]"); // parse arg
+        BacktrackParser parser = new BacktrackParser(lexer);
+        //System.out.println(parser.LT(11)); // can look far ahead
+        parser.stat(); // begin parsing at rule stat
+    }
 }
